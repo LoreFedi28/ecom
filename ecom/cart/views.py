688d5f315a9d869f30af636c721a0cfa7d7a230c
Lorenzo <fedilorenzogm@gmail.com>
2025-06-15 01:30:02 +0200

    Added Product Quantities

--- ecom/cart/views.py
+++ ecom/cart/views.py
@@ -1,3 +1,5 @@
+from statistics import quantiles
+
 from django.shortcuts import render, get_object_or_404
 
 from store.views import product
@@ -7,16 +9,20 @@ from django.http import JsonResponse
 from django.contrib import messages
 
 def cart_summary(request):
-    return render(request, "cart_summary.html", {})
+    cart = Cart(request)
+    cart_products = cart.get_prods
+    quantities = cart.get_quants
+    return render(request, "cart_summary.html", {"cart_products":cart_products, "quantities":quantities})
 
 def cart_add(request):
     cart = Cart(request)
     if request.POST.get('action') == 'post':
         product_id = int(request.POST.get('product_id'))
+        product_qty = int(request.POST.get('product_qty'))
 
         product = get_object_or_404(Product, id=product_id)
 
-        cart.add(product=product)
+        cart.add(product=product, quantity=product_qty)
 
         cart_quantity = cart.__len__()
 
