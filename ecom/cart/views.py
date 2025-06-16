8f0747dd2c486925d37094012b7d904c2eeaf53d
Lorenzo <fedilorenzogm@gmail.com>
2025-06-16 23:53:19 +0200

    Update Cart and remove items

--- ecom/cart/views.py
+++ ecom/cart/views.py
@@ -12,7 +12,8 @@ def cart_summary(request):
     cart = Cart(request)
     cart_products = cart.get_prods
     quantities = cart.get_quants
-    return render(request, "cart_summary.html", {"cart_products":cart_products, "quantities":quantities})
+    totals = cart.cart_total()
+    return render(request, "cart_summary.html", {"cart_products":cart_products, "quantities":quantities, "totals":totals})
 
 def cart_add(request):
     cart = Cart(request)
@@ -31,8 +32,25 @@ def cart_add(request):
         return response
 
 
-def cart_remove(request):
-    pass
+def cart_delete(request):
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('product_id'))
+
+        cart.delete(product=product_id)
+
+        response = JsonResponse({'product: ': product_id})
+        messages.success(request, ("Product Deleted..."))
+        return response
 
 def cart_update(request):
-    pass
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('product_id'))
+        product_qty = int(request.POST.get('product_qty', 1) or 1)
+
+        cart.update(product=product_id, quantity=product_qty)
+
+        response = JsonResponse({'qty: ': product_qty})
+        messages.success(request, ("Product Updated..."))
+        return response
