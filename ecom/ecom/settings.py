dcb6f7ef154b4ee4d09f7b7a86641fd4c77cdaa7
Lorenzo <fedilorenzogm@gmail.com>
2025-06-18 21:34:40 +0200

    added db password

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD_YO'),
+        'PASSWORD': os.environ['DB_PASSWORD_YO'],
         'HOST': os.environ.get('DB_HOST'),
         'PORT': os.environ.get('DB_PORT'),
     }
