b1bf4901a33841474cb0e08a0dfc06f6deff0546
Lorenzo <fedilorenzogm@gmail.com>
2025-06-18 23:26:16 +0200

    Added PayPal payments

--- ecom/settings.py
+++ ecom/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'cart',
     'payment',
     'whitenoise.runserver_nostatic',
+    'paypal.standard.ipn',
 ]
 
 MIDDLEWARE = [
@@ -133,3 +134,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Add PayPal settings
+# Set sandbox to true
+PAYPAL_TEST = True
+PAYPAL_RECEIVER_EMAIL = 'business@lorenzo.com' # Business Sandbox account
--- payment/views.py
+++ payment/views.py
@@ -6,6 +6,10 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from store.models import Product, Profile
 import datetime
+from django.urls import reverse
+from paypal.standard.forms import PayPalPaymentsForm
+from django.conf import settings
+import uuid # unique user id for duplicate orders
 
 def orders(request, pk):
     if request.user.is_authenticated and request.user.is_superuser:
@@ -173,13 +177,31 @@ def billing_info(request):
         my_shipping = request.POST
         request.session['my_shipping'] = my_shipping
 
+        # Get the host
+        host = request.get_host()
+        # Create a PayPal form
+        paypal_dict = {
+            'business': settings.PAYPAL_RECEIVER_EMAIL,
+            'amount': totals,
+            'item_name': 'Book Order',
+            'no_shipping': '2',
+            'invoice': str(uuid.uuid4()),  # Unique invoice ID
+            'currency_code': 'EUR',
+            'notify_url': 'https://{}{}'.format(host, reverse("paypal-ipn")),
+            'return_url': 'https://{}{}'.format(host, reverse("payment_success")),
+            'cancel_return': 'https://{}{}'.format(host, reverse("payment_failed")),
+        }
+
+        # Create actual PayPal button
+        paypal_form = PayPalPaymentsForm(initial=paypal_dict)
+
         # Check if the user is authenticated
         if request.user.is_authenticated:
             billing_form = PaymentForm()
-            return render(request, "payment/billing_info.html", {"cart_products": cart_products, "quantities": quantities, "totals": totals, "shipping_info": request.POST, "billing_form": billing_form})
+            return render(request, "payment/billing_info.html", {"paypal_form":paypal_form, "cart_products": cart_products, "quantities": quantities, "totals": totals, "shipping_info": request.POST, "billing_form": billing_form})
         else:
             billing_form = PaymentForm()
-            return render(request, "payment/billing_info.html", {"cart_products": cart_products, "quantities": quantities, "totals": totals, "shipping_info": request.POST, "billing_form": billing_form})
+            return render(request, "payment/billing_info.html", {"paypal_form":paypal_form, "cart_products": cart_products, "quantities": quantities, "totals": totals, "shipping_info": request.POST, "billing_form": billing_form})
 
         shipping_form = request.POST
         return render(request, "payment/billing_info.html", {"cart_products": cart_products, "quantities": quantities, "totals": totals, "shipping_form": shipping_form})
@@ -205,3 +227,6 @@ def checkout(request):
 
 def payment_success(request):
     return render(request, 'payment/payment_success.html', {})
+
+def payment_failed(request):
+    return render(request, 'payment/payment_failed.html', {})
